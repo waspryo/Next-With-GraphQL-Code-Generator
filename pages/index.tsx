493873ff297243411f9f82e345bac90b6a514326
杉本　諒 <wasp2123321@gmail.com>
2021-11-04 12:52:52 +0900

    get data from generated file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,27 @@
-import Head from "next/head";
+import Image from "next/image";
 
 import { getDataFromTree } from "@apollo/client/react/ssr";
-import { useQuery } from "@apollo/client";
-import { CHARACTERS_QUERY } from "../graphql/queries";
 import withApollo from "../lib/withApollo";
+import { useCharactersQuery } from "../generated";
 
 function Home() {
-  const { data } = useQuery(CHARACTERS_QUERY);
+  const { data, loading } = useCharactersQuery();
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
   return (
     <div>
       {data.characters.results.map((character) => (
-        <div>{character}</div>
+        <div key={character.id}>
+          <Image
+            src={character.image}
+            alt={character.name}
+            width="200px"
+            height="200px"
+          />
+          {character.name}
+        </div>
       ))}
     </div>
   );
