d801069620d963dc8fb3517a9d049677923746e8
杉本　諒 <wasp2123321@gmail.com>
2021-11-04 12:45:05 +0900

    generate code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,19 @@
 import Head from "next/head";
 
+import { getDataFromTree } from "@apollo/client/react/ssr";
 import { useQuery } from "@apollo/client";
 import { CHARACTERS_QUERY } from "../graphql/queries";
 import withApollo from "../lib/withApollo";
 
 function Home() {
   const { data } = useQuery(CHARACTERS_QUERY);
-  return <div>{JSON.stringify(data)}</div>;
+  return (
+    <div>
+      {data.characters.results.map((character) => (
+        <div>{character}</div>
+      ))}
+    </div>
+  );
 }
 
-export default withApollo(Home);
+export default withApollo(Home, { getDataFromTree });
